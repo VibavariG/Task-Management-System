d73efca857a23e77eb82c25a6f64ce1fac8ddbe8
Vibavari Gurunathan <vibavari2301@gmail.com>
2024-01-18 14:02:39 -0800

    Create User and Task Model

--- task-management-svc/src/main/java/com/project/taskmanagement/model/Task.java
+++ task-management-svc/src/main/java/com/project/taskmanagement/model/Task.java
@@ -1,4 +1,25 @@
 package com.project.taskmanagement.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
 public class Task {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String description;
+    private String author;
+    private LocalDate dueDate;
+    private String status = "Pending";
+    private LocalDateTime updatedOn = LocalDateTime.now();
 }
